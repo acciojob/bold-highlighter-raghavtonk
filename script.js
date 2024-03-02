1ce4b6247008baba0db99d6cc29230235b76f03d
Raghav Kumar Tonk <86098245+raghavtonk@users.noreply.github.com>
2024-03-02 13:12:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function highlight() {
     //Write your code here
-
+	let element = document.querySelectorAll("strong");
+	// element.classList.add("onmouse")
+	element.forEach((ele)=>{
+		// ele.style.color="green"
+		ele.classList.add("onmouse")
+	})
 
 }
 
 
 function return_normal() {
     //Write your code here
-
+	let element = document.querySelectorAll("strong");
+	element.forEach((ele)=>{
+		// ele.style.color="black"
+		ele.classList.remove("onmouse")
+	})
     
 }
